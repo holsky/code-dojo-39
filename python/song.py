b4005ac6f9e34d696d1929dc42312ca68b6a7bf9
Holger Schmeisky <google@schmeisky.com>
2017-03-23 22:23:07 +0100

    extract all stanzas

--- python/song.py
+++ python/song.py
@@ -3,47 +3,19 @@ import collections
 animal = collections.namedtuple('struct', 'name phrase')
 
 animals = [
-	animal("fly", "I don't know why she swallowed a {0} - perhaps she'll die!"),
+	animal("fly", "I don't know why she swallowed a fly - perhaps she'll die!"),
 	animal("spider", "That wriggled and wiggled and tickled inside her."),
-	animal("bird", "How absurd to swallow a {0}.")
+	animal("bird", "How absurd to swallow a bird."),
+	animal("cat", "Fancy that to swallow a cat!"),
+	animal("dog", "What a hog, to swallow a dog!"),
+	animal("cow", "I don't know how she swallowed a cow!")
 ]
 closing_animal = "horse"
 
 opening = "There was an old lady who swallowed a {0}"
-
 swallow_phrase = "She swallowed the {0} to catch the {1}"
-
-stanzas = """There was an old lady who swallowed a cat;
-Fancy that to swallow a cat!
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
-
-There was an old lady who swallowed a dog;
-What a hog, to swallow a dog!
-She swallowed the dog to catch the cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
-
-There was an old lady who swallowed a cow;
-I don't know how she swallowed a cow!
-She swallowed the cow to catch the dog,
-She swallowed the dog to catch the cat,
-She swallowed the cat to catch the bird,
-She swallowed the bird to catch the spider,
-She swallowed the spider to catch the fly;
-I don't know why she swallowed a fly - perhaps she'll die!
-
-"""
-
 closing = """There was an old lady who swallowed a {}...
 ...She's dead, of course!"""
-
-def format_animal_phrase(n):
-	return animals[n].phrase.format(animals[n].name)
 
 def format_swallow_phrase(n):
 	return swallow_phrase.format(animals[n].name, animals[n - 1].name)
@@ -53,18 +25,15 @@ def format_opening(n):
 
 def build_stanza(n):
 	stanza = format_opening(n) + "\n" \
-		+ format_animal_phrase(n) + "\n"
+		+ animals[n].phrase + "\n"
 	for i in range(n, 0, -1):
 		stanza += format_swallow_phrase(i) + ("," if i > 1 else ";") + "\n"
 	if n > 0:
-		stanza += format_animal_phrase(0) + "\n"
+		stanza += animals[0].phrase + "\n"
 	return stanza
 
 def original_song():
-	return build_stanza(0) + "\n" \
-		+ build_stanza(1) + "\n" \
-		+ build_stanza(2) + "\n" \
-		+ stanzas \
+	return ''.join([build_stanza(n) + "\n" for n in range(0, 6)]) \
 		+ closing.format(closing_animal)
 
 if __name__ == '__main__':
